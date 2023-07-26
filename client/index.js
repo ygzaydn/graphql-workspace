6066f9602bab4b8b678ebef1e66c2160a4103d6d
ygzaydn <ygzaydns@gmail.com>
2023-07-26 11:31:39 +0300

    added react-router

--- client/index.js
+++ client/index.js
@@ -2,16 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import SongList from "./components/songlist";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     uri: "http://localhost:4000/graphql",
 });
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <SongList />,
+    },
+]);
+
 const Root = () => {
     return (
         <ApolloProvider client={client}>
-            <SongList />
+            <RouterProvider router={router} />
         </ApolloProvider>
     );
 };
