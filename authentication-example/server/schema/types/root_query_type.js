82d7d2130616d1f585fa114a2b24f1ae4f83a1de
ygzaydn <ygzaydns@gmail.com>
2023-07-31 10:27:32 +0300

    minor fixes

--- authentication-example/server/schema/types/root_query_type.js
+++ authentication-example/server/schema/types/root_query_type.js
@@ -1,8 +1,9 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+    name: "RootQueryType",
+    fields: {},
 });
 
 module.exports = RootQueryType;
